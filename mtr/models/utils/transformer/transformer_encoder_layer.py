39dfc03a6749d8aa8d166ff153489c521b53b47e
pengzhenghao <pzh@cs.ucla.edu>
2023-04-08 21:58:57 -0700

    up

--- mtr/models/utils/transformer/transformer_encoder_layer.py
+++ mtr/models/utils/transformer/transformer_encoder_layer.py
@@ -29,7 +29,6 @@ def _get_activation_fn(activation):
 
     
 class TransformerEncoderLayer(nn.Module):
-
     def __init__(self, d_model, nhead, dim_feedforward=2048, dropout=0.1,
                  activation="relu", normalize_before=False, use_local_attn=False):
         super().__init__()
@@ -51,12 +50,11 @@ class TransformerEncoderLayer(nn.Module):
         self.dropout2 = nn.Dropout(dropout)
 
         self.activation = _get_activation_fn(activation)
-        self.normalize_before = normalize_before
 
     def with_pos_embed(self, tensor, pos: Optional[Tensor]):
         return tensor if pos is None else tensor + pos
 
-    def forward_post(self,
+    def forward(self,
                      src,
                      src_mask: Optional[Tensor] = None,
                      src_key_padding_mask: Optional[Tensor] = None,
@@ -76,40 +74,3 @@ class TransformerEncoderLayer(nn.Module):
         src = src + self.dropout2(src2)
         src = self.norm2(src)
         return src
-
-    def forward_pre(self, src,
-                    src_mask: Optional[Tensor] = None,
-                    src_key_padding_mask: Optional[Tensor] = None,
-                    pos: Optional[Tensor] = None, 
-                    index_pair=None, 
-                    query_batch_cnt=None, 
-                    key_batch_cnt=None, 
-                    index_pair_batch=None):
-        src2 = self.norm1(src)
-        q = k = self.with_pos_embed(src2, pos)
-        src2 = self.self_attn(q, k, value=src, attn_mask=src_mask,
-                              key_padding_mask=src_key_padding_mask, 
-                              index_pair=index_pair, query_batch_cnt=query_batch_cnt, 
-                              key_batch_cnt=key_batch_cnt, index_pair_batch=index_pair_batch)[0] 
-        src = src + self.dropout1(src2)
-        src2 = self.norm2(src)
-        src2 = self.linear2(self.dropout(self.activation(self.linear1(src2))))
-        src = src + self.dropout2(src2)
-        return src
-
-    def forward(self, src,
-                src_mask: Optional[Tensor] = None,
-                src_key_padding_mask: Optional[Tensor] = None,
-                pos: Optional[Tensor] = None, 
-                # for local-attn
-                index_pair=None, 
-                query_batch_cnt=None, 
-                key_batch_cnt=None, 
-                index_pair_batch=None):
-        if self.normalize_before:
-            return self.forward_pre(src, src_mask, src_key_padding_mask, pos, 
-                                    index_pair=index_pair, query_batch_cnt=query_batch_cnt, 
-                                    key_batch_cnt=key_batch_cnt, index_pair_batch=index_pair_batch)
-        return self.forward_post(src, src_mask, src_key_padding_mask, pos, 
-                                 index_pair=index_pair, query_batch_cnt=query_batch_cnt, 
-                                 key_batch_cnt=key_batch_cnt, index_pair_batch=index_pair_batch)
